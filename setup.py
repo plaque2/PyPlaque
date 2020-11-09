a88dfeb2800a9ec552250bdf37b49cc146a12d3b
Artur Yakimovich, PhD <artur.yakimovich@gmail.com>
2020-11-09 21:37:19 +0000

    minor version update

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ folder_path = path.abspath(path.dirname(__file__))
 with open(path.join(folder_path, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+version = 'v0.0.2'
+
 setup(
   name = 'PyPlaque',
   packages = ['PyPlaque'],
-  version = 'v0.0.2',
+  version = version,
   license='GPLv3',
   description = 'Python package for virus plaque analysis based on Plaque2.0',
   author = 'Plaque2.0 Team, AILS Institute',
   author_email = 'ayakimovich@ails.institute',
   url = 'https://github.com/plaque2/PyPlaque',
-  download_url = 'https://github.com/plaque2/PyPlaque/archive/v0.0.1.tar.gz',
+  download_url = 'https://github.com/plaque2/PyPlaque/archive/{}.tar.gz'.format(version),
   keywords = ['Virus', 'Plaque', 'Virology', 'Microscopy', 'Computer vision'],
   install_requires=[
           'scikit-image',
@@ -31,6 +33,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
   long_description=long_description,
     long_description_content_type='text/markdown'
