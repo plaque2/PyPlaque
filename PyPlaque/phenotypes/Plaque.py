676d1b86d36e72c4450c4d221b45e3bf5f1b80d8
de29 <t.de@hzdr.de>
2023-07-17 21:18:07 +0200

    scripts changes according to linting errors

--- PyPlaque/phenotypes/Plaque.py
+++ PyPlaque/phenotypes/Plaque.py
@@ -1,111 +1,121 @@
-import numpy as np
 import cv2
-from skimage.measure import label, regionprops, moments
+import numpy as np
+from skimage.measure import label, regionprops
+
 from PyPlaque.utils import check_numbers
 
+
 class Plaque:
+  """
+  **Plaque** class is designed to hold a single virological plaque
+  phenotype as an object.
+
+  _Arguments_:
+
+  mask - (required, 2D numpy array) containing binary mask of a single
+  virological plaque object.
+
+  centroid - (float tuple, optional) contains x and y of the centroid of the
+  plaque object
+
+  bbox - (float tuple, optional) contains minr, minc, maxr, maxc of the
+  plaque object
+  """
+
+  def __init__(self, mask, centroid = None, bbox = None):
+    #check data types
+    if (not isinstance(mask, np.ndarray)) or (not mask.ndim == 2):
+      raise TypeError("Mask atribute of Plaque must be a 2D numpy array")
+
+    self.mask = mask
+    self.area = regionprops(label(mask))[0].area
+
+    if centroid:
+      if (not isinstance(centroid, tuple)) or check_numbers(centroid):
+        raise TypeError("centroid must be a tuple of coordinates")
+      self.centroid = centroid
+    if bbox:
+      if (not isinstance(bbox, tuple)) or check_numbers(bbox):
+        raise TypeError("Bounding box must be a tuple of limits")
+      self.bbox = bbox
+
+
+  def measure(self):
     """
-    **Plaque** class is designed to hold a single virological plaque
-    phenotype as an object.
+    **measure method** returns for an individual plaque object,the area of the
+    bbox surrounding a plaque, and an approximation of the actual area based on
+    the proportion of white pixels in the mask.
 
     _Arguments_:
-
-    mask - (required, 2D numpy array) containing binary mask of a single
-    virological plaque object.
-
-    centroid - (float tuple, optional) contains x and y of the centroid of the
-    plaque object
-
-    bbox - (float tuple, optional) contains minr, minc, maxr, maxc of the
-    plaque object
     """
+    plq_bbox_area = (self.bbox[3] - self.bbox[1]) * (self.bbox[2]
+    - self.bbox[0]) # assuming bbox = (minr, minc, maxr, maxc)
+    number_of_white_pix = np.sum(self.mask > 0)  # extracting non-white pixels
+    plq_area = number_of_white_pix
 
-    def __init__(self, mask, centroid = None, bbox = None):
-        #check data types
-        if (not type(mask) is np.ndarray) or (not mask.ndim == 2):
-            raise TypeError("Mask atribute of Plaque must be a 2D numpy array")
+    return plq_bbox_area, plq_area
 
-        self.mask = mask
-        self.area = regionprops(label(mask))[0].area
+  def eccentricity(self):
+    """
+    **eccentricity method** returns for an individual plaque object,the
+    eccentricity of the plaque which is found by fitting an ellipse to the
+    plaque boundary and finding the eccentricity given by sqrt(1-(b^2/a^2))
+    where b is the length of the semi-minor axis and a is the length of the
+    semi-major axis
 
-        if centroid:
-            if (not type(centroid) is tuple) or check_numbers(centroid):
-                raise TypeError("centroid must be a tuple of coordinates")
-            self.centroid = centroid
-        if bbox:
-            if (not type(bbox) is tuple) or check_numbers(bbox):
-                raise TypeError("Bounding box must be a tuple of limits")
-            self.bbox = bbox
+    _Arguments_:
+    """
+    # find the contours
+    contours,_ = cv2.findContours(self.mask, cv2.RETR_TREE,
+    cv2.CHAIN_APPROX_SIMPLE)
+    # print("Number of contours detected:", len(contours))
 
-
-    def measure(self):
-        """
-        **measure method** returns for an individual plaque object,the area of the bbox surrounding a plaque,
-        and an approximation of the actual area based on the proportion of white pixels in the mask.
-
-        _Arguments_:
-        """
-        plq_bbox_area = (self.bbox[3] - self.bbox[1]) * (self.bbox[2] - self.bbox[0]) # assuming bbox = (minr, minc, maxr, maxc)
-        number_of_white_pix = np.sum(self.mask > 0)  # extracting non-white pixels 
-        plq_area = number_of_white_pix
-
-        return plq_bbox_area, plq_area 
-
-    def eccentricity(self):
-        """
-        **eccentricity method** returns for an individual plaque object,the eccentricity of the plaque which is
-        found by fitting an ellipse to the plaque boundary and finding the eccentricity given by sqrt(1-(b^2/a^2))
-        where b is the length of the semi-minor axis and a is the length of the semi-major axis
-
-        _Arguments_:
-        """
-        # find the contours
-        contours,_ = cv2.findContours(self.mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        # print("Number of contours detected:", len(contours))
-
-        ecc = 0
-        # select the first contour that has more than 5 points and fit an ellipse based on that
-        if len(contours) != 0:
-            for i in range(len(contours)):
-                if len(contours[i]) >= 5:
-                    # fit the ellipse
-                    ellipse=cv2.fitEllipse(contours[i])
-                    if ellipse[2] == 0: #if rotation angle is zero results are not reliable
-                        ecc = 0
-                    else:
-                        semi_major_axis = ellipse[1][0]/2
-                        semi_minor_axis = ellipse[1][1]/2
-                        # print("semi_minor_axis:",semi_minor_axis)
-                        # print("semi_major_axis:",semi_major_axis)
-
-                        if semi_minor_axis > semi_major_axis:
-                            temp = semi_minor_axis
-                            semi_minor_axis = semi_major_axis
-                            semi_major_axis = temp
-
-                        if semi_minor_axis == 0:
-                            semi_minor_axis = 0.1
-                        if semi_major_axis == 0:
-                            semi_major_axis = 0.1
-                        ecc = np.sqrt(1-(semi_minor_axis**2/semi_major_axis**2))
-                        break
-        else:
+    ecc = 0
+    # select the first contour that has more than 5 points and fit an ellipse
+    # based on that
+    if len(contours) != 0:
+      for i in range(len(contours)):
+        if len(contours[i]) >= 5:
+          # fit the ellipse
+          ellipse=cv2.fitEllipse(contours[i])
+          if ellipse[2] == 0: #if rotation angle is 0 results are not reliable
             ecc = 0
+          else:
+            semi_major_axis = ellipse[1][0]/2
+            semi_minor_axis = ellipse[1][1]/2
+            # print("semi_minor_axis:",semi_minor_axis)
+            # print("semi_major_axis:",semi_major_axis)
 
-        return ecc
+            if semi_minor_axis > semi_major_axis:
+              temp = semi_minor_axis
+              semi_minor_axis = semi_major_axis
+              semi_major_axis = temp
 
-    def roundness(self):
-        """
-        **roundness method** returns for an individual plaque object,the roundness of the plaque which is
-        found by the following ratio given by 4 * pi * Area / ( Perimeter^2 ) where Area is 4 * pi * radius^2
-        and Perimeter is 2 * pi * radius.
+            if semi_minor_axis == 0:
+              semi_minor_axis = 0.1
+            if semi_major_axis == 0:
+              semi_major_axis = 0.1
+            ecc = np.sqrt(1-(semi_minor_axis**2/semi_major_axis**2))
+            break
+    else:
+      ecc = 0
 
-        _Arguments_:
-        """
-        _, plq_area = self.measure()
-        point1 = np.array((self.bbox[3],self.bbox[2]))
-        point2 = np.array(((self.bbox[3]+self.bbox[1])/2,(self.bbox[2]+self.bbox[0])/2))
-        radius = np.linalg.norm(point1 - point2)
-        perimeter = 2 * np.pi * radius
-        ratio = 4 * np.pi * plq_area / ( perimeter ** 2 )
-        return ratio
+    return ecc
+
+  def roundness(self):
+    """
+    **roundness method** returns for an individual plaque object,the roundness
+    of the plaque which is found by the following ratio given by 4 * pi * Area /
+     ( Perimeter^2 ) where Area is 4 * pi * radius^2
+    and Perimeter is 2 * pi * radius.
+
+    _Arguments_:
+    """
+    _, plq_area = self.measure()
+    point1 = np.array((self.bbox[3],self.bbox[2]))
+    point2 = np.array(((self.bbox[3]+self.bbox[1])/2,(self.bbox[2]+
+    self.bbox[0])/2))
+    radius = np.linalg.norm(point1 - point2)
+    perimeter = 2 * np.pi * radius
+    ratio = 4 * np.pi * plq_area / ( perimeter ** 2 )
+    return ratio
--- PyPlaque/specimen/PlateImage.py
+++ PyPlaque/specimen/PlateImage.py
@@ -1,157 +1,164 @@
-import numpy as np
 import os
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops
-import matplotlib.pyplot as plt
+
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.measure import label, regionprops
+from skimage.segmentation import clear_border
+
 
 class PlateImage:
+  """
+  **PlateImage Class** is aimed to contain a full multititre plate image and
+  it's respective binary mask.
+
+  _Arguments_:
+
+  n_rows - (int, required) number of rows in the plate (usually lower than
+  the number of rows).
+
+  n_columns - (int, required) number of columns in the plate (usually higher
+  than the number of rows).
+
+  plate_image - (np.array, required) an image of individual wells of the
+  plate.
+
+  plate_mask - (np.array, required) a binary mask outlining individual wells of
+  the plate.
+
+  inverted - (bool, required) an indicator of whether the plate.
+
+  It is advisable to have both dimensions of plate masks and images be 2 dim
+  """
+  def __init__(self,
+                n_rows,
+                n_columns,
+                plate_image,
+                plate_mask,
+                inverted = False):
+    #check data types
+    if not isinstance(n_rows, int):
+      raise TypeError('Expected n_rows argument to be int')
+    if not isinstance(n_columns, int):
+      raise TypeError('Expected n_columns argument to be int')
+    if (not isinstance(plate_image, np.ndarray)) or (not (plate_image.ndim >=2
+    and plate_image.ndim <= 3)):
+      raise TypeError('Image atribute of the plate must be a 2D numpy array')
+    if (not isinstance(plate_mask, np.ndarray)) or (not plate_mask.ndim == 2):
+      raise TypeError('Mask atribute of the plate must be a 2D numpy array')
+    if not isinstance(inverted, bool):
+      raise TypeError('inverted atribute of the plate must be of type bool')
+
+    self.n_rows = n_rows
+    self.n_columns = n_columns
+    self.plate_image = plate_image
+    self.plate_mask = plate_mask
+    self.inverted = inverted
+
+  def get_wells(self, min_area = 100):
     """
-    **PlateImage Class** is aimed to contain a full multititre plate image and
-    it's respective binary mask.
-
-    _Arguments_:
-
-    n_rows - (int, required) number of rows in the plate (usually lower than
-    the number of rows).
-
-    n_columns - (int, required) number of columns in the plate (usually higher than
-    the number of rows).
-
-    plate_image - (np.array, required) an image of individual wells of the
-    plate.
-
-    plate_mask - (np.array, required) a binary mask outlining individual wells of the
-    plate.
-
-    inverted - (bool, required) an indicator of whether the plate.
-
-    It is advisable to have both dimensions of plate masks and images be 2 dim
+    **get_wells method** returns a list of individual wells of the plate
+    stored as binary numpy arrays.
     """
-    def __init__(self, n_rows, n_columns, plate_image, plate_mask, inverted = False):
-        #check data types
-        if not type(n_rows) is int:
-            raise TypeError("Expected n_rows argument to be int")
-        if not type(n_columns) is int:
-            raise TypeError("Expected n_columns argument to be int")
-        if (not type(plate_image) is np.ndarray) or ( not (plate_image.ndim >=2 and plate_image.ndim <= 3)):
-            raise TypeError("Image atribute of the plate must be a 2D numpy array")
-        if (not type(plate_mask) is np.ndarray) or (not plate_mask.ndim == 2):
-            raise TypeError("Mask atribute of the plate must be a 2D numpy array")
-        if not type(inverted) is bool:
-            raise TypeError("inverted atribute of the plate must be of type bool")
+    well_crops = []
+    for _,well in enumerate(regionprops(label(clear_border(self.plate_mask)))):
+      if well.area >= min_area:
+        minr, minc, maxr, maxc = well.bbox
+        masked_img = self.plate_image ** self.plate_mask
+        well_crops.append(masked_img[minr:maxr, minc:maxc])
+    return well_crops
 
-        self.n_rows = n_rows
-        self.n_columns = n_columns
-        self.plate_image = plate_image
-        self.plate_mask = plate_mask
-        self.inverted = inverted
+  def get_well_positions(self, min_area = 100):
+    """
+    **get_well_positions method** returns a list of individual wells of the
+    plate stored as binary numpy arrays along with a number with rows numbered
+    starting from 1 and columns numbered starting from 1.
+    """
+    well_dict = {}
+    well_crops = []
+    lc_zip = []
+    for idx,well in enumerate(regionprops(label(clear_border(self.plate_mask)))
+    ):
+      if well.area >= min_area:
+        minr, minc, maxr, maxc = well.bbox
+        masked_img = self.plate_image ** self.plate_mask
+        well_crops.append(masked_img[minr:maxr, minc:maxc])
+        lc_zip.append((maxr,minc))
+        # minc, maxr make up the top right corner of the bounding box that
+        # encloses the well
+        # We order by minc and maxr together
 
-    def get_wells(self, min_area = 100):
-        """
-        **get_wells method** returns a list of individual wells of the plate
-        stored as binary numpy arrays.
-        """
-    # ToDo: automated calculation of well row and col from x,y position
-        well_crops = []
-        for idx,well in enumerate(regionprops(label(clear_border(self.plate_mask)))):
-            if well.area >= min_area:
-                minr, minc, maxr, maxc = well.bbox
-                masked_img = self.plate_image ** self.plate_mask
-                well_crops.append(masked_img[minr:maxr, minc:maxc])
-        return well_crops
+        well_dict[idx] = {}
+        well_dict[idx]['masked_img'] = masked_img[minr:maxr, minc:maxc]
+        well_dict[idx]['mask'] = self.plate_mask[minr:maxr, minc:maxc]
+        well_dict[idx]['img'] = self.plate_image[minr:maxr, minc:maxc]
+        well_dict[idx]['maxr'] = maxr
+        well_dict[idx]['minc'] = minc
+        well_dict[idx]['minr'] = minr
+        well_dict[idx]['maxc'] = maxc
 
-    def get_well_positions(self, min_area = 100):
-        """
-        **get_well_positions method** returns a list of individual wells of the plate
-        stored as binary numpy arrays along with a number with rows numbered starting from 1 and columns numbered starting from 1.
-        """
-        well_dict = {}
-        well_crops = []
-        lc_zip = []
-        for idx,well in enumerate(regionprops(label(clear_border(self.plate_mask)))):
-            if well.area >= min_area:
-                minr, minc, maxr, maxc = well.bbox
-                masked_img = self.plate_image ** self.plate_mask
-                well_crops.append(masked_img[minr:maxr, minc:maxc])
-                lc_zip.append((maxr,minc))
-                # minc, maxr make up the top right corner of the bounding box that encloses the well
-                # We order by minc and maxr together
+    l = lc_zip
+    if not self.inverted:
+      r_no = 1
+      c_no = 1
+    else:
+      r_no = 1
+      c_no = self.n_columns
 
-                well_dict[idx] = {}
-                well_dict[idx]['masked_img'] = masked_img[minr:maxr, minc:maxc]
-                well_dict[idx]['mask'] = self.plate_mask[minr:maxr, minc:maxc]  
-                well_dict[idx]['img'] = self.plate_image[minr:maxr, minc:maxc]
-                well_dict[idx]['maxr'] = maxr
-                well_dict[idx]['minc'] = minc
-                well_dict[idx]['minr'] = minr
-                well_dict[idx]['maxc'] = maxc
+    while len(l)>0:
+      x_sorted = sorted(l, key=lambda tup: tup[1])
+      column = x_sorted[:self.n_rows]
+      y_sorted = sorted(column, key=lambda tup: tup[0])
 
-        l = lc_zip
-        if self.inverted == False:
-            r_no = 1
-            c_no = 1
-        else:
-            r_no = 1
-            c_no = self.n_columns
+      if not self.inverted:
+        for (maxr,minc) in y_sorted:
+          temp  = [k for k,v in well_dict.items() if ((int(v['maxr'])
+          == maxr) and (int(v['minc']) == minc))]
+          well_dict[temp[0]]['nrow'] = r_no
+          well_dict[temp[0]]['ncol'] = c_no
+          r_no +=1
 
-        while(len(l)>0):
-            x_sorted = sorted(l, key=lambda tup: tup[1])
-            column = x_sorted[:self.n_rows]
-            y_sorted = sorted(column, key=lambda tup: tup[0])
+        c_no +=1
+        r_no = 1
+      else:
+        for (maxr,minc) in y_sorted:
+          temp  = [k for k,v in well_dict.items() if ((int(v['maxr'])
+          == maxr) and (int(v['minc']) == minc))]
+          well_dict[temp[0]]['nrow'] = r_no
+          well_dict[temp[0]]['ncol'] = c_no
+          r_no +=1
 
-            if self.inverted == False:
-                for (maxr,minc) in y_sorted:
-                    temp  = [k for k in well_dict.keys() if ((int(well_dict[k]['maxr']) == maxr) and (int(well_dict[k]['minc']) == minc))]
-                    well_dict[temp[0]]['nrow'] = r_no
-                    well_dict[temp[0]]['ncol'] = c_no
-                    r_no +=1
+        c_no -=1
+        r_no = 1
 
-                c_no +=1
-                r_no = 1
-            else:
-                for (maxr,minc) in y_sorted:
-                    temp  = [k for k in well_dict.keys() if ((int(well_dict[k]['maxr']) == maxr) and (int(well_dict[k]['minc']) == minc))]
-                    well_dict[temp[0]]['nrow'] = r_no
-                    well_dict[temp[0]]['ncol'] = c_no
-                    r_no +=1
-
-                c_no -=1
-                r_no = 1
-
-            l = x_sorted[self.n_rows:]
+      l = x_sorted[self.n_rows:]
 
 
-        return well_dict
+    return well_dict
 
-    def plot_well_positions(self,save=True, folder_path = '../data/results'):
-        """
-        **plot_well_positions method** plot boxes around individual wells of the plate (inferred from plate mask),
-        with rows numbered starting from 1 and columns numbered starting from 1.
-        """ 
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.imshow(self.plate_mask)
+  def plot_well_positions(self,save=True, folder_path = '../data/results'):
+    """
+    **plot_well_positions method** plot boxes around individual wells of
+    the plate (inferred from plate mask), with rows numbered starting from 1
+      and columns numbered starting from 1.
+    """
+    _, ax = plt.subplots(figsize=(10, 6))
+    ax.imshow(self.plate_mask)
 
-        well_dict = self.get_well_positions()
+    well_dict = self.get_well_positions()
 
-        for idx in well_dict.keys():
-            rect = mpatches.Rectangle((well_dict[idx]['minc']-50, well_dict[idx]['minr']-50), (well_dict[idx]['maxc']+50) - (well_dict[idx]['minc']-50), 
-                                (well_dict[idx]['maxr']+50) - (well_dict[idx]['minr']-50), fill=False, edgecolor='white', linewidth=2)
-            ax.add_patch(rect)
+    for _,v in well_dict.items():
+      rect = mpatches.Rectangle((v['minc']-50,v['minr']-50), (v['maxc']+50) -
+      (v['minc']-50),(v['maxr']+50) - (v['minr']-50),fill=False,
+      edgecolor='white', linewidth=2)
+      ax.add_patch(rect)
 
-            ax.annotate(str(well_dict[idx]['nrow'])+","+str(well_dict[idx]['ncol']), xy =(well_dict[idx]['minc'], well_dict[idx]['maxr']),color='white')
-        ax.set_axis_off()
-        plt.title("Annotated Wells")
-        plt.tight_layout()
-        if save==True:
-            plt.savefig(os.path.join(folder_path,'output.svg'),bbox_inches='tight', dpi=300)
-        plt.show()       
-
-    def get_well_positions_from_source(self, min_area = 100):
-        """
-        **get_wells_positions_from_source method** returns a list of individual wells of the plate (inferred from plate image),
-        stored as binary numpy arrays along with a number with rows numbered starting from 1 and columns numbered starting from 1.
-        """     
-        return     
-
-
+      ax.annotate(str(v['nrow'])+','+str(v['ncol']), xy =(v['minc'],v['maxr']),
+      color='white')
+    ax.set_axis_off()
+    plt.title('Annotated Wells')
+    plt.tight_layout()
+    if save:
+      plt.savefig(os.path.join(folder_path,'output.svg'),
+      bbox_inches='tight', dpi=300)
+    plt.show()
