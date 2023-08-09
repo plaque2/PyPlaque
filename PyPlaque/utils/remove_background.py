988efd4e3bb6a6844ebfb073373c6e59f75cfc95
de29 <t.de@hzdr.de>
2023-08-09 13:31:48 +0200

    removing change for type error python 3.8 build

--- PyPlaque/utils/remove_background.py
+++ PyPlaque/utils/remove_background.py
@@ -2,8 +2,8 @@ import numpy as np
 from skimage import restoration
 
 
-def remove_background(img: np.ndarray, radius: float) -> tuple(np.ndarray,
-np.ndarray):
+def remove_background(img: np.ndarray, radius: float) -> tuple[np.ndarray,
+np.ndarray]:
   img = np.array(img).astype(np.uint16)
 
   background = restoration.rolling_ball(img, radius=radius)
