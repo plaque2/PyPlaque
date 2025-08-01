faf09d7fffd5f8869e8aa270875bb710aa40c837
Artur Yakimovich, PhD <arthur.yakimovitch@gmail.com>
2025-08-01 16:56:26 +0200

    HOTFIX: Update setup.py
    remove the trailing space in 'Topic :: Scientific/Engineering :: Bio-Informatics '

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
   classifiers=[
      'Development Status :: 3 - Alpha',
      'Intended Audience :: Science/Research',
-     'Topic :: Scientific/Engineering :: Bio-Informatics ',
+     'Topic :: Scientific/Engineering :: Bio-Informatics',
      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
      'Programming Language :: Python :: 3.9',
      'Programming Language :: Python :: 3.10',
